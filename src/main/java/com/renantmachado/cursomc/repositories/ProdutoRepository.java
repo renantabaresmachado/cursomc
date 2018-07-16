c6b0c0fcea943c10baac97d9c32cd74c7c66d4b7
renantabaresmachado <renantmachado@savef5.com.br>
2018-07-16 11:49:09 -0300

    Produto e assossia~cao muitos para muitos

---
+++ src/main/java/com/renantmachado/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.renantmachado.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.renantmachado.cursomc.domain.Produto;
+
+@Repository
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+
+}
