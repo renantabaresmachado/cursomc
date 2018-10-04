69b5213deae9d230c6469a59d56e0f31da3d2def
renantabaresmachado <renantmachado@savef5.com.br>
2018-10-04 00:12:58 -0300

    Deletendo uma categoria com DELETE

--- src/main/java/com/renantmachado/cursomc/services/CategoriaService.java
+++ src/main/java/com/renantmachado/cursomc/services/CategoriaService.java
@@ -2,11 +2,14 @@ package com.renantmachado.cursomc.services;
 
 import java.util.Optional;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.renantmachado.cursomc.domain.Categoria;
 import com.renantmachado.cursomc.repositories.CategoriaRepository;
+import com.renantmachado.cursomc.services.exceptions.DataIntegrityException;
 import com.renantmachado.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +33,16 @@ public class CategoriaService {
 	public Categoria update (Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException ex){
+			throw new DataIntegrityException("Não é possivel exclluir uma categoria que possui produtos assiciados a ela");
+		}
 	}
 	
 
